8a3a8e5a5d0078cc71fb546b84e12459e31bba59
Pontus Carlsson <pontus@thefarm.se>
2013-09-24 10:05:46 +0200

    * Restructured the JSON mapping * Added extra fields * Reversed the exception and frames order

---
+++ src/main/java/se/thefarm/SentryHandler.java
@@ -0,0 +1,38 @@
+package se.thefarm;
+
+import android.app.Application;
+import org.acra.ACRA;
+import se.thefarm.acra.senders.SentrySender;
+
+public class SentryHandler implements Thread.UncaughtExceptionHandler {
+
+    public static SentryHandler instance;
+
+    private static Throwable latestThrowable;
+
+    public static void init(Application application) {
+        instance = new SentryHandler(application);
+    }
+
+    public SentryHandler(Application application) {
+        ACRA.init(application);
+
+        String dsn = ACRA.getConfig().formUri();
+
+        ACRA.getErrorReporter().setReportSender(new SentrySender(dsn));
+
+        Thread.setDefaultUncaughtExceptionHandler(this);
+    }
+
+
+    @Override
+    public void uncaughtException(Thread thread, Throwable throwable) {
+        latestThrowable = throwable;
+
+        ACRA.getErrorReporter().uncaughtException(thread, throwable);
+    }
+
+    public static Throwable getLatestThrowable() {
+        return latestThrowable;
+    }
+}
